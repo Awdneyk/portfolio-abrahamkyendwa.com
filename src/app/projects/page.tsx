996dc91490450a2867d37fe62984ec5f88382160
Awdneyk <abrahamkyendwa@gmail.com>
2025-06-09 18:56:08 +0300

    Revert "adding lint"
    This reverts commit bee6510e0db2c0edb20631daed0f66c4515328c6.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-//import Image from "next/image";
+import Image from "next/image";
 import ProjectCard from "@/components/ProjectCard";
 import PageTransition from "@/components/PageTransition";
 
